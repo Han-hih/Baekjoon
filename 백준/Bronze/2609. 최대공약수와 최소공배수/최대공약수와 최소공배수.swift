0b8dde7edbe8cadb48ccd07f5dc123c34a449039
황인호 <109748526+Han-hih@users.noreply.github.com>
2024-06-01 20:29:02 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 16 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let nums = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+   if b == 0 {
+	  return a
+   } else {
+	  return gcd(b, a % b)
+   }
+}
+
+func lcm(_ a: Int, _ b: Int) -> Int {
+   return a * b / gcd(a, b)
+}
+
+print(gcd(nums[0], nums[1]))
+print(lcm(nums[0], nums[1]))
