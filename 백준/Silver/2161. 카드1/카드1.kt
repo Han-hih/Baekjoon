0e93f0d794ce165f7580cd45dac9f7181add22ac
황인호 <109748526+Han-hih@users.noreply.github.com>
2024-09-13 14:52:09 +0900

    [Silver V] Title: 카드1, Time: 116 ms, Memory: 21316 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.kt
@@ -0,0 +1,21 @@
+import java.util.LinkedList
+import java.util.Queue
+
+fun main() {
+    val n = readln().toInt()
+    val queue: Queue<Int> = LinkedList()
+    val result = mutableListOf<Int>()
+
+    for (i in 1..n) {
+        queue.add(i)
+    }
+
+    while (queue.size > 1) {
+        result.add(queue.poll())
+        queue.add(queue.poll())
+    }
+
+    result.add(queue.poll())
+
+    println(result.joinToString(" "))
+}
